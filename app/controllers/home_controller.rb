eaf652d5947ce16d52c4a83cecb45e2f3579f150
Pradeep Agrawal <pradeeprordev@gmail.com>
2019-07-18 21:07:46 +0530

    Added sql query for getting search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,7 @@ class HomeController < ApplicationController
   end
 
   def search
-    data = [
-      { text: Procedure.order('RANDOM()').first.title, url: '/example' },
-      { text: Provider.order('RANDOM()').first.full_name, url: '/example' }
-    ]
+    data = Procedure.search(params[:query])
     render json: { data: data, query: params[:query] }
   end
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,2 +1,36 @@
 class Procedure < ApplicationRecord
+
+  def self.search(keyword)
+    self.find_by_sql([sql_query, "#{keyword}%"])
+  end
+
+  def self.sql_query
+    query = <<-SQL
+      SELECT title, url FROM (
+        select
+        'procedures',
+        p.title as title,
+        '/example' as url,
+        'A' as sequence
+       from procedures p
+      union
+        select
+        'providers',
+        concat(provider.first_name, ' ', provider.last_name) as title,
+        '/example' as url,
+        'B' as sequence
+       from providers as provider
+      union
+        select
+        'destinations',
+        concat(destination.city, ' ', destination.country) as title,
+        '/example' as url,
+        'C' as sequence
+        from destinations as destination
+        )
+      searchable
+      where searchable.title ILIKE ?
+      ORDER BY sequence;
+    SQL
+  end
 end
